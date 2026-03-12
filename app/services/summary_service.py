6fbae73768c965edec5fa52b56894118747909e6
Priyanka Thakur <ppriyankathakur223@gmail.com>
2026-03-12 10:23:25 +0530

    final

--- app/services/summary_service.py
+++ app/services/summary_service.py
@@ -3,7 +3,6 @@ from transformers import pipeline
 # load summarization model
 summarizer = pipeline("text-generation", model="google/flan-t5-small")
 
-
 def generate_summary(transcript):
 
     prompt = f"Summarize the following meeting discussion:\n{transcript}"
